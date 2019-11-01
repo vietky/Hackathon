4d4746f856d357e86ac089f274158fb1cb45a3c0
Ky Quoc Viet <vietky@Kys-MacBook-Pro.local>
2019-11-02 01:17:30 +0700

    init ad insert

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import App from './AppTemp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
