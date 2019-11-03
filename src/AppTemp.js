5326a70dbc4ce302db61690ccf1022c04da60a40
Cong Le <congle@chotot.vn>
2019-11-03 23:51:22 +0700

    fix conflic

--- src/AppTemp.js
+++ src/AppTemp.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import AdInsert from './AdInsert';
-import AdListing from './components/AdListing.js';
-import AdDetail from './components/AdDetail.js';
+import AdListing from './AdListing';
 import AdView from './AdView';
 import {
   BrowserRouter as Router,
@@ -34,22 +33,42 @@ export default function BasicExample() {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div className="AdListing">
-      <nav id="nav-main" class="navbar fixed-top navbar-expand-lg navbar-light bg-main">
-        <div class="navbar-toggler">
-          <img id="img-logo" src={require("./components/images/logo.png")} alt="Responsive image" height="30" />
-        </div>
-        <a class="navbar-brand no-padding no-margin" href="#">
-          <img src="https://static.chotot.com/storage/marketplace/common/searchActive.svg" height="18" />
-          <img src="https://static.chotot.com/storage/marketplace/common/notificationActive.svg" height="18" />
-          <img src="https://static.chotot.com/storage/chotot-icons/svg/chat.svg" alt="chat" height="18" />
-          <img src="https://static.chotot.com/storage/marketplace/common/userActive.svg" height="18" />
-          <img src="https://static.chotot.com/storage/marketplace/common/moreActive.svg" height="7" />
-          <button id="btn-post" class="btn btn-warn">ĐĂNG TIN</button>
-        </a>
-      </nav>
-      <AdListing />
-    </div>
+    <div className="container">
+      <Router>
+        <Navbar className="navbar" color="light" light expand="md">
+          <NavbarBrand href="/">Sugar</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/adview">Adview</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/adinsert">AdInsert</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
 
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <AdListing />
+          </Route>
+          <Route path="/adview">
+            <AdView />
+          </Route>
+          <Route path="/adinsert">
+            <AdInsert />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
