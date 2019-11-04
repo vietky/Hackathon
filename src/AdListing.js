5e8c1893d94abca615b510137363aa4f8e30ee53
Cong Le <congle@chotot.vn>
2019-11-04 12:04:36 +0700

    add Ad component

--- src/AdListing.js
+++ src/AdListing.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import MusicPlayer from './components/MusicPlayer';
+import Ad from './components/Ad';
 import './components/css/adlix.css';
 import './components/css/main.css';
 
@@ -127,34 +128,7 @@ class AdListing extends Component {
         {/* <!-- AD LISTING --> */}
         <div id="ad-listing" className="container-fluid">
           {this.state.tracks.map((item, index) => (
-            <div className="ad-itemx flex-nowrap row" key={index}>
-              <div className="ad-itemx__image col-4">
-                <img src={item.images[0]} alt="" />
-                <svg className="ad-itemx__image-player" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M128 104.3v303.4c0 6.4 6.5 10.4 11.7 7.2l240.5-151.7c5.1-3.2 5.1-11.1 0-14.3L139.7 97.2c-5.2-3.3-11.7.7-11.7 7.1z" /></svg>
-              </div>
-
-              <div className="ad-itemx__detail col-8">
-                <p className="ad-itemx__detail__title">
-                  {item.title}
-                </p>
-                <div className="ad-itemx__detail-bottom">
-                  <div className="ad-itemx__detail__price text-dred">
-                    {item.price}
-                  </div>
-                  <div className="ad-itemx__detail__tilo">
-                    <img width="20" src="/images/icon-bag.svg" alt="icon-bag" />
-                    &nbsp;
-                    <span className="just-a-slice"></span>
-                    &nbsp;
-                    <span className="ad-itemx__detail__timestamp">1 phút trước</span>
-                    &nbsp;
-                    <span className="just-a-slice"></span>
-                    &nbsp;
-                    <span className="ad-itemx__detail__location">Tp Hồ Chí Minh</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Ad item={item} key={index} />
           ))}
 
         </div>
